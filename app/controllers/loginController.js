cf608cb6701832cba6391fabb01fc0c4ebdf8c2e
hrubio <hectorrubio@grupopromessa.com>
2023-07-21 16:38:05 -0600

    body validation

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -2,6 +2,7 @@ const bycrypt = require('bcrypt');
 const UserRepository = require('../repository/User');
 const loginController = async (req, res) => {
   const {email, password} = req.body
+  if(!email || !password) return res.status(400).send('email and password are required')
   //login with email and password with sequelize 
   const user = await new UserRepository().findByEmail(email);
   if(!user) return res.status(404).send('user not found');
